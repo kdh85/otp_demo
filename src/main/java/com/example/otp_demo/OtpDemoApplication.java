07057c8587fb0a7ce2d23f4a0692ec8a400df0f6
kdh <modudamba@naver.com>
2023-01-25 16:26:29 +0900

    * feat : java OtpDemoApplication 구현.

---
+++ src/main/java/com/example/otp_demo/OtpDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.otp_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OtpDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OtpDemoApplication.class, args);
+    }
+
+}
